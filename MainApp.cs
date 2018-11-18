efaa2b44992cc14d3dd81cd5d6e26fe71b44ad60
Jangyeongwoo <hphphp23@naver.com>
2018-11-18 22:48:10 +0900

    MazeProgram

---
+++ MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeProgram
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            Maze maze = new Maze();
+            maze.load("a.txt");
+            PrintMaze print = new PrintMaze();
+            print.print();
+
+            StartMaze start = new StartMaze();
+            maze.searchExit();
+            print.print();
+
+
+
+
+
+        }
+    }
+}
